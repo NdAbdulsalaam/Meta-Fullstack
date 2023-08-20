1f2486eb4a0662b115db01a3ac7551d25dee4ea5
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-08-20 08:45:13 +0100

    add dommy data to model

--- django/myproject/myapp/models.py
+++ django/myproject/myapp/models.py
@@ -1,13 +1,25 @@
 from django.db import models
-from django.utils import dates
+# from django.utils import timezone
 
 # Create your models here.
 class Members(models.Model):
+    MemberCat = (
+        ("1", "New"),
+        ("2", "Silver"),
+        ("3", "Gold"),
+        ("4", "Diamond")
+        )
+    
     MemberID = models.IntegerField(primary_key=True)
+    MemberType = models.CharField(max_length=12, choices=MemberCat, default='1')
     FirstName = models.CharField(max_length=20)
     LastName = models.CharField(max_length=20)
+    Email = models.EmailField(null=False, default="admin@gmail.com")
     DateOfBirth = models.DateField()
-    Joined = models.DateTimeField()
+    Joined = models.DateTimeField(null=True)
+    
+    class Meta:
+        db_table = "Members"
     
 
     
