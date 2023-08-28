5a0ff55f3faeeac8c31b1e317332d48f2afac5bf
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-08-28 01:26:10 +0100

    about to use MySQL DB

--- django/myproject/myproject/settings.py
+++ django/myproject/myproject/settings.py
@@ -76,25 +76,26 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': BASE_DIR / 'mydb',
-#         # 'USER' : BASE_DIR / env('DB_USER'),
-#         # 'PASSWORD' : env('DB_PASSWORD'),
-#         # 'HOST' : env('DB_HOST'),
-#         'HOST' : 'localhost',
-#         # 'PORT' : env('DB_PORT')
-#         'PORT': 3306      
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {   
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'mydatabase',   
+        'USER': 'root',   
+        'PASSWORD': '',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
+} 
 
 
 # Password validation
