39dbed80b7aab1f5cd9e01bdfc1ada13861c571a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-20 17:39:10 +0100

    add 'return' before JsonResponse under views.books' 'except' block

--- API/BookList/BookListAPI/views.py
+++ API/BookList/BookListAPI/views.py
@@ -29,7 +29,7 @@ def books(request):
     try:
         book.save()
     except IntegrityError:
-        JsonResponse(
+        return JsonResponse(
             {'error':'true','message':'required field missing'},status=400
         )
     
