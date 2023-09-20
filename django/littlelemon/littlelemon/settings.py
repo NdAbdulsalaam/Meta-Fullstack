0e85d9dc53895ce25051a5a5796e617d6e8d1087
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-20 16:31:13 +0100

    add empty ALLOWED_HOSTS to settings.py

--- django/littlelemon/littlelemon/settings.py
+++ django/littlelemon/littlelemon/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = []
 
 # DEBUG = False
 # ALLOWED_HOSTS = ['*']
