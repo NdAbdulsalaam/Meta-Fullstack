0cb8e340149ec577f20318bde773503125398288
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-20 14:56:39 +0100

    add app: BookListAPI

--- API/BookList/BookList/settings.py
+++ API/BookList/BookList/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'BookList.urls'
@@ -120,7 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'productionfiles'
 
+# STATICFILES_DIRS = [
+#     "restaurant/static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
