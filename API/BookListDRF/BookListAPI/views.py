539e485e5f453c7f4bcd5058419926db25aa86e3
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-21 13:13:55 +0100

    Install django-restframework

--- API/BookListDRF/BookListAPI/views.py
+++ API/BookListDRF/BookListAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view()
+def books(request):
+    return Response("Not sure of what i'm doing yet", 
+                    status=status.HTTP_200_OK)
--- API/BookListDRF/BookListDRF/settings.py
+++ API/BookListDRF/BookListDRF/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'BookListAPI',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'BookListDRF.urls'
@@ -121,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
