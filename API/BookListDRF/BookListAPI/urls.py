0c5195c759a635ac2d5feeef50eb8dab196dfdf3
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 03:22:47 +0100

    add urls for MenuItemsView and comment

--- API/BookListDRF/BookListAPI/urls.py
+++ API/BookListDRF/BookListAPI/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('books/', views.BookView.as_view(), name='BookView'),
     path('books/<int:pk>/', views.SingleBookView.as_view(), name='SingleBookView'),
+    # path('menu-items', views.MenuItemsView.as_view()),
+    # path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
