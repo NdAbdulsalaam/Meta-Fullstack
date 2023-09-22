8e7aaf5fc19a8b2a0e26bdb20eba1f4492e1daaf
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 10:37:35 +0100

    Update model.py to include category

--- API/BookListDRF/BookListAPI/models.py
+++ API/BookListDRF/BookListAPI/models.py
@@ -10,10 +10,18 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} (by: {self.author})"
 
-class MenuItem(models.Model):
+class Category(models.Model):
+    slug = models.SlugField()
     title = models.CharField(max_length=225)
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+class MenuItem(models.Model):
+    title = models.SlugField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     
     def __str__(self):
         return f"{self.title} (avail: {self.inventory})"
