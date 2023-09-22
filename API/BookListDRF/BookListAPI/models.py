21d20f4642c147ace0e11577818d2e061ee0c912
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 03:34:47 +0100

    uncomment '__str__(self)' for readable model names in the models.py

--- API/BookListDRF/BookListAPI/models.py
+++ API/BookListDRF/BookListAPI/models.py
@@ -7,5 +7,5 @@ class Book(models.Model):
     author = models.CharField(max_length=225)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     
-    # def __str__(self):
-    #     return f"{self.title} (by: {self.author})"
+    def __str__(self):
+        return f"{self.title} (by: {self.author})"
