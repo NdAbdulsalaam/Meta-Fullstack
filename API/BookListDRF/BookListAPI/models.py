0457dfa257d1209ed8c7fa31d20c0a845824a3bd
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 03:37:37 +0100

    add MenuItem model

--- API/BookListDRF/BookListAPI/models.py
+++ API/BookListDRF/BookListAPI/models.py
@@ -9,3 +9,8 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} (by: {self.author})"
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=225)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
