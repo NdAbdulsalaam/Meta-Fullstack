ce30910bb641cdec33cd3855a55b6162dcb66b7b
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 10:47:26 +0100

    Update serializer.py for the category model

--- API/BookListDRF/BookListAPI/serializer.py
+++ API/BookListDRF/BookListAPI/serializer.py
@@ -5,11 +5,18 @@ class BookSerializer(seria.ModelSerializer):
     class Meta:
         model = models.Book
         fields = ['id', 'title', 'author', 'price']
+    
+class CategorySerializer(seria.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ['id','title']
         
 class MenuItemSerializer(seria.ModelSerializer):
+    category_id = seria.IntegerField(write_only = True)
+    category = CategorySerializer(read_only = True)
     class Meta:
         model = models.MenuItem
-        fields = ['id', 'title', 'price', 'inventory']
+        fields = ['id', 'title', 'price', 'inventory', 'category', 'category_id']
         
     extra_kwargs = {
         'price': {
@@ -19,8 +26,3 @@ class MenuItemSerializer(seria.ModelSerializer):
             'min_value': 0
         }
     }
-    
-# class CategorySerializer (serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ['id','title']
