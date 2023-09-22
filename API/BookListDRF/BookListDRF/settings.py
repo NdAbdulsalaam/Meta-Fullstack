3924a35ad9b64453051669ac6ca8bc633767194a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 12:50:55 +0100

    fix bugs in the REST_FRAMEWORK dict

--- API/BookListDRF/BookListDRF/settings.py
+++ API/BookListDRF/BookListDRF/settings.py
@@ -143,9 +143,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
                                   ],
     
-    'DEFAULT_PAGINATION_CLASS': [
-        'rest_framework.pagination.PageNumberPagination',
-        ],
-    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
 }
