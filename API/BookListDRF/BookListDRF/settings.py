777dcf08e55c3b507df985b22518af6ea3165ba2
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 04:07:06 +0100

    comment rest_framework\_xml renderers

--- API/BookListDRF/BookListDRF/settings.py
+++ API/BookListDRF/BookListDRF/settings.py
@@ -132,6 +132,7 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES' : ['rest_framework.renderers.JSONRenderer',
-                                  'rest_framework.renderers.BrowsableAPIRenderer',
-                                  'rest_framework\_xml.renderers.XMLRenderer']
+                                  'rest_framework.renderers.BrowsableAPIRenderer'
+                                #   'rest_framework\_xml.renderers.XMLRenderer'
+                                  ]
 }
