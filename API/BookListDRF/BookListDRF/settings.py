c3a8bdc6d59c7820e35e88d2ee4f1f58eec5092f
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2023-09-22 11:01:13 +0100

    Update settings.py for pagination

--- API/BookListDRF/BookListDRF/settings.py
+++ API/BookListDRF/BookListDRF/settings.py
@@ -132,10 +132,20 @@ INTERNAL_IPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES' : ['rest_framework.renderers.JSONRenderer',
-                                  'rest_framework.renderers.BrowsableAPIRenderer',
-                                  'rest_framework_xml.renderers.XMLRenderer',
-                                  'rest_framework.filters.OrderingFilter',
-                                  'rest_framework.filters.SearchFilter',
-                                  ]
+    'DEFAULT_RENDERER_CLASSES' : [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        ],
+    
+    'DEFAULT_FILTER_CLASSES' : [ 
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+                                  ],
+    
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.pagination.PageNumberPagination',
+        ],
+    
+    'PAGE_SIZE': 2,
 }
